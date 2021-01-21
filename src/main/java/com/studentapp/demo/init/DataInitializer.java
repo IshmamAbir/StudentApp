8d8a33449c7b61105f5876949929d644e4f7d3da
Ishmam Abir <ishmam130@gmail.com>
2021-01-21 23:44:53 +0600

    data initialize code

---
+++ src/main/java/com/studentapp/demo/init/DataInitializer.java
@@ -0,0 +1,13 @@
+package com.studentapp.demo.init;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Data Initialised");
+
+    }
+}
