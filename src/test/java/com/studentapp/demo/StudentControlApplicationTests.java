f182469816e30843040963e9611f07cfe2567ac7
Ishmam Abir <ishmam130@gmail.com>
2021-01-21 13:51:57 +0600

    first commit

---
+++ src/test/java/com/studentapp/demo/StudentControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentapp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
